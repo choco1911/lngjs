2d6fbc119b937216b774fdbd1b68083fe3d332a6
choco <choco@rambler-co.ru>
2018-09-01 13:38:40 +0300

    add state

--- src/Article.js
+++ src/Article.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-// props common approach to translate data in React
-// functional component
-export default function Article(props) {
-    const {article} = props
-    return (
-        <div>
-            <h3>{article.title}</h3>
-            <section>{article.text}</section>
-        </div>
-    )
+export default class Article extends Component {
+    //  let's create state
+    constructor(props) {
+        // constructor of parent class
+        super(props) // what for to pass props into super ?
+        // initial state of component
+        this.state = {
+            isOpen: false
+        }
+
+        // use bind if toggleOpen is regular function
+        // this.toggleOpen = this.toggleOpen.bind(this)
+    }
+
+    render() {
+        const {article} = this.props
+        const {isOpen} = this.state
+        const section = isOpen ? <section> {article.text} </section> : null
+        return ( <div>
+                    <h3> {article.title} </h3>
+                    <button onClick = {this.toggleOpen}></button>
+                    {section}
+                </div>
+        )
+    }
+/*
+    toggleOpen() {
+            this.setState({
+                    isOpen: !this.state.isOpen
+            })
+    }
+*/
+// is arrow function using Lexical Scope to find this - variable?
+    toggleOpen = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
 }
