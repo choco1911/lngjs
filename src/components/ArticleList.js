269a77f1191e42d7cdf8b7dd4bd4b3c812b1cc3c
choco <choco@rambler-co.ru>
2018-10-02 23:54:55 +0300

    open/close article in verbose mode

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,14 +4,18 @@ import Article from './Article'
 
 export default class ArticleList extends Component {
     state = {
-        openArticleId: true
+        openArticleId: null,
+        switchCommiter: false
     }
+                // isOpen = {article.id === this.state.openArticleId && this.state.stArt}
 
     render() {
         const articleElements = this.props.articles.map(article => <li key={article.id}>
+             {console.log('expr ---in render--- ', article.id === this.state.openArticleId )}
+             {console.log('switcher ---in render--- ',  this.state.switchCommiter) }
             <Article
                 article = {article}
-                isOpen = {article.id === this.state.openArticleId}
+                isOpen = {article.id === this.state.openArticleId && this.state.switchCommiter}
                 toggleOpen = {this.toggleOpenArticle(article.id)}
             />
         </li> )
@@ -24,6 +28,20 @@ export default class ArticleList extends Component {
     }
 
     toggleOpenArticle = (openArticleId) => ev => {
-        this.setState({ openArticleId })
+        console.log('expr ---in function--- ', openArticleId === this.state.openArticleId )
+        console.log('switcher ---in function--- ',  this.state.switchCommiter)
+
+        /*
+        if (openArticleId !== this.state.openArticleId) {
+            this.setState({switchCommiter: true})
+        } else {
+            this.setState({switchCommiter: !this.state.switchCommiter})
+        }
+        */
+
+        this.setState({ openArticleId,
+            //switchCommiter: (openArticleId !== this.state.openArticleId ? true : !this.state.switchCommiter)
+            switchCommiter: openArticleId !== this.state.openArticleId ? true : !this.state.switchCommiter
+         })
     }
 }
